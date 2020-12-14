1f5548f5bdc437d2363adf5136c8503163a8c253
Mau <mauridrani95@gmail.com>
2020-12-14 20:57:02 -0300

    Solved missing elements in Hero

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -6,7 +6,24 @@ import image from "../../Assets/Img Hero/Ic_ilustra_Hero@3x.png";
 const Hero = () => {
   return (
     <div className="hero">
-      
+      <Enunciate
+        containerClass=" enunciate-container"
+        enunciateClass=" enunciate-big column-flex-end-center fw-5"
+        content={
+          <span>
+            Bienvenido a tu{" "}
+            <span className="fw-3 fc-blue">Entrevista Técnica</span> en{" "}
+            <span className="fw-2 fc-green">Wolox</span>
+          </span>
+        }
+      />
+
+      <Banner
+        alt="hero-image"
+        containerClass=" row-start-flex-start img-container"
+        imgClass="  img-hero row-flex-start-flex-start"
+        src={image}
+      />
     </div>
   );
 };
